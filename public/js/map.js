53f1a4463bf9b7b79f9b8dbe6d88ccefac11bb5f
BaoPhu <baophu@rionlab.com>
2019-12-04 19:37:14 +0700

    UPdate CV

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,9 @@
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(16.066837, 108.191848),
+        center: new google.maps.LatLng(16.071962, 108.196276),
         zoom: 16,
     };
-    var uluru = {lat: 16.066837, lng: 108.191848};
+    var uluru = {lat: 16.071962, lng: 108.196276};
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
     var marker = new google.maps.Marker({
         position: uluru,
